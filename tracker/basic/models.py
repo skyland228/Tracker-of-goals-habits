63c04584eec113a40633a1bc36dee56edb799be2
sky <skyland22@yandex.ru>
2025-09-19 20:26:17 +0300

    Добавил статусы для общей модели и для временной

---
+++ tracker/basic/models.py
@@ -0,0 +1,77 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from datetime import date
+
+class Habit(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(
+        get_user_model(),on_delete=models.SET_NULL,related_name='habits',null=True,verbose_name='Пользователь',)
+    created_at = models.DateField(auto_now_add=True)
+    goal = models.ForeignKey('TemporalGoal',on_delete=models.SET_NULL,
+                             related_name='habits', null = True, blank = True,
+                             verbose_name='Цель,') # мы записываем, какой цели следует эта привычка
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+class HabitStatus(models.Model):
+
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE, related_name='habit_statuses')
+    is_completed = models.BooleanField(default=False)
+    date = models.DateField(default=date.today)
+
+    objects = models.Manager()
+    class Meta:
+        unique_together = ('habit', 'date')
+
+
+class GeneralGoal(models.Model):
+    name =  models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.SET_NULL,
+        related_name='general_goals', null=True, verbose_name='Пользователь', )
+    is_completed = models.BooleanField(default=False)  # Добавляем поле is_completed
+    theme = models.CharField(max_length=50,blank = True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+class GeneralStatus(models.Model):
+    goal = models.ForeignKey(GeneralGoal, on_delete=models.CASCADE, related_name='GeneralStuses' )
+    is_completed = models.BooleanField(default=False)
+    date = models.DateField(default = date.today)
+
+    objects = models.Manager()
+    class Meta:
+        unique_together = ('goal', 'date')
+
+class TemporalGoal(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.SET_NULL,
+        related_name='temporal_goals', null=True, verbose_name='Пользователь')
+    deadline = models.DateField(blank = True, default='1999-11-22')  # Обязательное поле срока выполнения
+    is_completed = models.BooleanField(default=False)  # Добавляем поле is_completed
+    general_goal = models.ForeignKey(GeneralGoal,on_delete = models.SET_NULL,
+                                     related_name='temporal_goal', null=True,
+                                     blank=True, verbose_name='Основная цель')
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+class TemporalStatus(models.Model):
+    goal = models.ForeignKey(TemporalGoal, on_delete=models.CASCADE, verbose_name='temporalstatus')
+    date = models.DateField(default = date.today)
+    is_completed = models.BooleanField(default=False)
+
+    objects = models.Manager()
+
+    class Meta:
+        unique_together = ('goal','date')
